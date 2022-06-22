4dcf6b8f20eb7605e99a829fd7d5858927977c2a
GamGyu <barry0105@Naver.com>
2022-06-22 21:18:28 +0900

    0622

--- React/react-study02/src/component/Survey/Page.js
+++ React/react-study02/src/component/Survey/Page.js
@@ -1,47 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './page.css';
+import INFP from '../Result/INFP.js';
 function Page1(props) {
-    const Question = {
-        1 : '안녕하세요',
-        2 : '반갑습니다',
-        3 : '고마워요',
-        4 : '감사해요',
-        5 : '잘있어요',
-        6 : '다시 만나요'
-    };
-    const Answer = {
-        'E' : 'E용 대답',
-        'I' : 'I용 대답',
-        'S' : 'S용 대답',
-        'N' : 'N용 대답'
-    };
+    var [result,AppendingResult] = useState('');
+
+    function ResultAppend(Append){
+        AppendingResult(result+=Append);
+        PageSlide();
+    }
+    const Question = [
+        {
+            'id' : 0,
+            'Question' : '질문 1',
+            'Answer' : ['대답 1','대답 2'],
+            'Values' : ['I','E']
+        },
+        {
+            'id' : 1,
+            'Question' : '질문 2',
+            'Answer' : ['대답 1','대답 2'],
+            'Values' : ['S','N']
+        },
+        {
+            'id' : 2,
+            'Question' : '질문 3',
+            'Answer' : ['대답 1','대답 2'],
+            'Values' : ['F','P']
+        },
+        {
+            'id' : 3,
+            'Question' : '질문 4',
+            'Answer' : ['대답 1','대답 2'],
+            'Values' : ['T','J']
+        }];
+    const createQuestion = 
+            Question.map((li,idx)=>
+                (<div className='Question'>
+                    
+                    <img src='img/NoPic.png'></img>
+                    <h2>{li.Question}</h2>
+                    <div className='survey-wrap'>
+                    <div className='survey-unit' >
+                        <input type='radio' name={idx} id={li.id}/>
+                        <label for={li.id}>
+                        <span></span>
+                        <p onClick={()=>{ResultAppend(li.Values[0])}}>{li.Answer[0]}</p>
+                        </label>
+                    </div>
+                    <div className='survey-unit' >
+                        <input type='radio' name={idx} id={li.id+1} value={li.Values[1]}/>
+                        <label for={li.id+1}>
+                        <span></span>
+                        <p onClick={()=>{ResultAppend(li.Values[1])}}>{li.Answer[1]}</p>
+                        </label>
+                    </div>
+                    </div>
+                </div>
+                )  
+            );
+    let [Page,PageChange] = useState(0);    
+    const PageSlide = (() =>{
+            if(Page < Question.length-1){
+                PageChange(Page+1);
+            }
+            else{
+                document.write(result);
+            }
+            
+        });
     return (
         <div className='Background'>
-            <div className='Question'>
-                <img src='img/NoPic.png'></img>
-                <h2>포도 안녕 미쁘다 우리는 함초롱하다 도담도담 그루잠 도르레 다솜 우리는 미쁘다</h2>
-            </div>
-            <hr/>
-            <div className='survey-wrap'>
-            <div className='survey-unit' name='1'>
-                <input type='radio' id='radio1'/>
-                <label for='radio1'>
-                <span></span>
-                <p>하이?</p>
-                </label>
-            </div>
-            <div className='survey-unit' name='1'>
-                <input type='radio' id='radio2'/>
-                <label for='radio3'>
-                <span></span>
-                <p>바이?</p>
-                </label>
-            </div>
-            </div>
-            <div className='Button-wrap'>
-                <button className='Back'>뒤로</button>
-                <button className='Next'>다음</button>
-            </div>
+            <button onClick={()=>{console.log(result)}}>결과 확인</button>
+            {createQuestion[Page]}
         </div>
     );
 }
