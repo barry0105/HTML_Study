8f14af768afaf30596307cb7e94e62713506ab3b
GamGyu <barry0105@Naver.com>
2022-06-23 20:26:38 +0900

    항복

--- React/react-study02/src/component/Survey/Page.js
+++ React/react-study02/src/component/Survey/Page.js
@@ -1,12 +1,15 @@
-import React, { useState } from 'react';
+import React, { useReducer, useState } from 'react';
 import './page.css';
 import INFP from '../Result/INFP.js';
 function Page1(props) {
     var [result,AppendingResult] = useState('');
-
     function ResultAppend(Append){
         AppendingResult(result+=Append);
         PageSlide();
+    }
+    function Countresult(e){
+        let Count = result.split(e).length - 1;
+        return Count;
     }
     const Question = [
         {
@@ -18,55 +21,54 @@ function Page1(props) {
         {
             'id' : 1,
             'Question' : '질문 2',
-            'Answer' : ['대답 1','대답 2'],
-            'Values' : ['S','N']
+            'Answer' : ['대답 1','대답 2','대답 3'],
+            'Values' : ['E','I','J']
         },
         {
             'id' : 2,
             'Question' : '질문 3',
             'Answer' : ['대답 1','대답 2'],
-            'Values' : ['F','P']
+            'Values' : ['E','I']
         },
         {
             'id' : 3,
             'Question' : '질문 4',
-            'Answer' : ['대답 1','대답 2'],
-            'Values' : ['T','J']
+            'Answer' : ['대답 1','대답 2','대답 3'],
+            'Values' : ['S','N','F']
         },{
             'id' : 4,
             'Question' : '질문 5',
             'Answer' : ['대답 1','대답 2'],
-            'Values' : ['바보','천재']
+            'Values' : ['N','S']
         },{
             'id' : 5,
             'Question' : '질문 6',
             'Answer' : ['대답 1','대답 2'],
-            'Values' : ['하이','바이']
+            'Values' : ['T','F']
         },];
     const createQuestion = 
             Question.map((li,idx)=>
-                (<div className='Question'>
-                    
+                (<div className='Question'> 
                     <img src='img/NoPic.png'></img>
                     <h2>{li.Question}</h2>
                     <div className='survey-wrap'>
-                    <div className='survey-unit' >
-                        <input type='radio' name={idx} id={li.id}/>
-                        <label for={li.id} onClick={()=>{ResultAppend(li.Values[0])}}>
-                        <span></span>
-                        <p >{li.Answer[0]}</p>
-                        </label>
+                    <div className='survey-unit'>
+                    <input type='radio' name={li.id} id={li.id}/>
+                    <label for={li.id} onClick={()=>{ResultAppend(li.Values[0])}}>
+                    <span></span>
+                    <p >{li.Answer[0]}</p>
+                    </label>
                     </div>
-                    <div className='survey-unit' >
-                        <input type='radio' name={idx} id={li.id+1} value={li.Values[1]}/>
-                        <label for={li.id+1} onClick={()=>{ResultAppend(li.Values[1])}}>
-                        <span></span>
-                        <p >{li.Answer[1]}</p>
-                        </label>
-                    </div>
+                    <div className='survey-unit'>
+                    <input type='radio' name={li.id} id={li.id}/>
+                    <label for={li.id} onClick={()=>{ResultAppend(li.Values[1])}}>
+                    <span></span>
+                    <p >{li.Answer[1]}</p>
+                    </label>
                     </div>
                 </div>
-                )  
+                </div>
+                )
             );
     let [Page,PageChange] = useState(0);    
     const PageSlide = (() =>{
@@ -74,7 +76,14 @@ function Page1(props) {
                 PageChange(Page+1);
             }
             else{
-                document.write(result);
+                document.write(' I : '+Countresult('I'));
+                document.write(' E : '+Countresult('E'));
+                document.write(' N : '+Countresult('N'));
+                document.write(' S : '+Countresult('S'));
+                document.write(' F : '+Countresult('F'));
+                document.write(' T : '+Countresult('T'));
+                document.write(' P : '+Countresult('P'));
+                document.write(' J : '+Countresult('J'));
             }
             
         });
